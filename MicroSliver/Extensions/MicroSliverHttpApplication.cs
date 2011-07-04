c4f4ca6c5b01c698d6a4f3140e2ef154fdffec03
jbillmann <none@none.com>
2011-07-04 23:36:56 +0000

    Stumped out web extension

---
+++ MicroSliver/Extensions/MicroSliverHttpApplication.cs
@@ -0,0 +1,28 @@
+﻿// 
+// Author: Jeremiah Billmann <jbillmann@gmail.com>
+// Copyright (c) 2011
+// 
+// Licensed under the Microsoft Public License (Ms-PL).
+// 
+using System;
+using System.Web;
+
+namespace MicroSliver.Web.Extensions
+{
+    public abstract class MicroSliverHttpApplication : HttpApplication
+    {
+        private static IIoC _ioc;
+        public IIoC IoC
+        {
+            get
+            {
+                return _ioc;
+            }
+        }
+
+        public void LoadIoC(IIoC ioc)
+        {
+            _ioc = ioc;
+        }
+    }
+}
