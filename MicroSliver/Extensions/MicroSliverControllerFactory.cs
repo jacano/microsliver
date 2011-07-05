4820cd3f918229077c8a8df4e6546ac8f72e79b1
jbillmann <none@none.com>
2011-07-05 03:18:59 +0000

    WIP: mvc extensions..

---
+++ MicroSliver/Extensions/MicroSliverControllerFactory.cs
@@ -0,0 +1,27 @@
+﻿//// 
+//// Author: Jeremiah Billmann <jbillmann@gmail.com>
+//// Copyright (c) 2011
+//// 
+//// Licensed under the Microsoft Public License (Ms-PL).
+//// 
+//using System;
+//using System.Web.Mvc;
+//using System.Web.Routing;
+
+//namespace MicroSliver.Web.Extensions
+//{
+//    public class MicroSliverControllerFactory : IControllerFactory
+//    {
+//        private IIoC IoC;
+
+//        public MicroSliverControllerFactory(IIoC ioc)
+//        {
+//            IoC = ioc;
+//        }
+
+//        public IController CreateController(RequestContext context, Type controllerType)
+//        {
+//            return (IController)IoC.GetByType(controllerType);
+//        }
+//    }
+//}
---
+++ MicroSliver/Extensions/MicroSliverDomainFactory.cs
@@ -0,0 +1,37 @@
+﻿// 
+// Author: Jeremiah Billmann <jbillmann@gmail.com>
+// Copyright (c) 2011
+// 
+// Licensed under the Microsoft Public License (Ms-PL).
+// 
+using System;
+using System.ServiceModel.DomainServices.Server;
+
+namespace MicroSliver.Web.Extensions
+{
+    public class MicroSliverDomainFactory : IDomainServiceFactory
+    {
+        private IIoC IoC;
+        public MicroSliverDomainFactory(IIoC ioc)
+        {
+            IoC = ioc;
+        }
+
+        public DomainService CreateDomainService(Type domainServiceType, DomainServiceContext context)
+        {
+            DomainService domainService = (DomainService)IoC.GetByType(domainServiceType);
+
+            if (domainService != null)
+            {
+                domainService.Initialize(context);
+            }
+
+            return domainService;
+        }
+
+        public void ReleaseDomainService(DomainService domainService)
+        {
+            domainService.Dispose();
+        }
+    }
+}
